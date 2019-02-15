81acead7a4c4d259fa65660b74db53042284d449
apple <apple@appledeMacBook-Pro.local>
2019-02-15 22:45:45 +0800

    面向对象babel

---
+++ 02/dest/1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = 12;
+var b = 1;
+
+var show = function show() {
+  return a + b;
+};
+
+console.log(show(a, b));
---
+++ 02/src/1.js
@@ -0,0 +1,4 @@
+let a = 12;
+let b = 1;
+let show = ()=> a+b;
+console.log(show(a,b));
